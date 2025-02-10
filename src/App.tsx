b77b2031776e68a978fa603104bc07048d65c552
junjuny0227 <junjuny0227@gmail.com>
2025-02-10 23:32:41 +0900

    feat: 데이터 가져오기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const getData = async () => {
+  const { data } = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return data;
+};
+
 const App = () => {
-  return <h1>TanStack Query</h1>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getData,
+    staleTime: 5 * 60 * 1000,
+    gcTime: 30 * 60 * 1000,
+  });
+
+  if (isLoading) return <h1>로딩 중</h1>;
+  if (error) return <h1>에러: {error.message}</h1>;
+
+  return (
+    <div>
+      <h1>TanStack Query</h1>
+      {data?.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
