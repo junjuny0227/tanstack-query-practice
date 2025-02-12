513234d8edf4135e04f8ac3ea5e4ab8cec591496
junjuny0227 <junjuny0227@gmail.com>
2025-02-12 16:35:12 +0900

    feat: 데이터 전송하기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 
 type Article = {
   userId: number;
   id: number;
+  title: string;
+  body: string;
+};
+
+type ArticleInput = {
   title: string;
   body: string;
 };
@@ -15,13 +21,44 @@ const getData = async () => {
   return data;
 };
 
+const postData = async ({ title, body }: ArticleInput) => {
+  const { data } = await axios.post<Article>(
+    "https://jsonplaceholder.typicode.com/posts",
+    {
+      title,
+      body,
+      userId: 1,
+    }
+  );
+  return data;
+};
+
+const queryClient = new QueryClient();
+
 const App = () => {
+  const [title, setTitle] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+
   const { data, isLoading, error, refetch } = useQuery({
     queryKey: ["articles"],
     queryFn: getData,
     staleTime: 5 * 60 * 1000,
     gcTime: 30 * 60 * 1000,
   });
+
+  const { mutate } = useMutation({
+    mutationFn: postData,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["articles"] });
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate({ title, body });
+    setTitle("");
+    setBody("");
+  };
 
   if (isLoading) return <h1>로딩 중</h1>;
   if (error) return <h1>에러: {error.message}</h1>;
@@ -30,6 +67,21 @@ const App = () => {
     <div>
       <h1>TanStack Query</h1>
       <button onClick={() => refetch()}>refetch</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="title"
+        />
+        <input
+          type="text"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          placeholder="body"
+        />
+        <button>post</button>
+      </form>
       {data?.map((article) => (
         <div key={article.id}>
           <h2>{article.title}</h2>
